1a23d1dc5016e0f27a7e1e7c6145d1c8b8067efb
Li Mattson <liamattson@gmail.com>
2023-10-24 19:10:27 -0600

    contact form validation

---
+++ src/utils/validateContactForm.js
@@ -0,0 +1,32 @@
+export const validateContactForm = (values) => {
+    const errors = {};
+
+    if (!values.firstName) {
+        errors.firstName = 'Required';
+    } else if (values.firstName.length < 2) {
+        errors.firstName = 'Must be at least 2 characters.';
+    } else if (values.firstName.length > 15) {
+        errors.firstName = 'Must be 15 chrarcters or less';
+    }
+
+
+    if (!values.lastName) {
+        errors.lastName = 'Required';
+    } else if (values.lastName.length < 2) {
+        errors.lastName = 'Must be at least 2 characters.';
+    } else if (values.lastName.length > 15) {
+        errors.lastName = 'Must be 15 chrarcters or less';
+    }
+
+    const reg = /^\d+$/;
+    if (!reg.test(values.phoneNum)) {
+        errors.phoneNum = 'The phone number should only contain numbers.';
+
+    }
+
+    if (!values.email.includes('@')) {
+        errors.email = 'Email should contain an @';
+    }
+    
+    return errors;
+}
